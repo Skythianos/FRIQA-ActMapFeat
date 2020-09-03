5f4c9620ace719f26d7ad4a66a14f486c18b5517
Skythianos <33064388+Skythianos@users.noreply.github.com>
2020-09-03 12:07:01 +0200

    Update crossDatabaseTest.m

--- crossDatabaseTest.m
+++ crossDatabaseTest.m
@@ -31,27 +31,27 @@ path = '/home/domonkos/Desktop/QualityAssessment/Databases/kadid10k/images'; % P
 
 disp('MDID Feature Extraction');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-pathDist = '/home/domonkos/Desktop/QualityAssessment/Databases/MDID/distortion_images';
-pathRef  = '/home/domonkos/Desktop/QualityAssessment/Databases/MDID/reference_images';
+pathDist = '/home/domonkos/Desktop/QualityAssessment/Databases/MDID/distortion_images'; % PATH MDID
+pathRef  = '/home/domonkos/Desktop/QualityAssessment/Databases/MDID/reference_images';  % PATH MDID
 [FeaturesMDID] = getFeaturesMDID(pathDist, pathRef);
 
 load CSIQ.mat
-ModelTrainedCSIQ     = getModel(FeaturesCSIQ, cell2mat(dmos));
+ModelTrainedCSIQ     = getModel(FeaturesCSIQ, cell2mat(dmos)); % Model trained on CSIQ
 
 load TID2008_Data.mat
-ModelTrainedTID2008  = getModel(FeaturesTID2008, dmos);
+ModelTrainedTID2008  = getModel(FeaturesTID2008, dmos); % Model trained on TID2008
 
 load TID2013_Data.mat
-ModelTrainedTID2013  = getModel(FeaturesTID2013, dmos);
+ModelTrainedTID2013  = getModel(FeaturesTID2013, dmos); % Model trained on TID2013
 
 load VCL_FER.mat
-ModelTrainedVCLFER   = getModel(FeaturesVCLFER, mos);
+ModelTrainedVCLFER   = getModel(FeaturesVCLFER, mos); % Model trained on VCL@FER
 
 load KADID_Data2.mat
-ModelTrainedKADID10k = getModel(FeaturesKADID10k, dmos);
+ModelTrainedKADID10k = getModel(FeaturesKADID10k, dmos); % Model trained on KADID-10k
 
 load MDID.mat
-ModelMDID = getModel(FeaturesMDID, mos);
+ModelMDID = getModel(FeaturesMDID, mos); % Model trained on MDID
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp('Cross database testing model trained on KADID-10k');
